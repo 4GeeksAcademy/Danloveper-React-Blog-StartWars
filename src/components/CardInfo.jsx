e04e8e660d405f5a34fcd4e134478dc56fda37e5
Danloveper <danloveper@gmail.com>
2025-11-13 19:01:41 +0000

    Add/add paragraph in description

--- src/components/CardInfo.jsx
+++ src/components/CardInfo.jsx
@@ -13,7 +13,10 @@ const CardInfo = (props) => {
                 </div>
                 <div className="col-6 text-center my-auto">
                     <h1>{result.properties.name}</h1>
-                    <p>{result.description}</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio aperiam id nesciunt temporibus sequi nam earum accusantium doloribus sint vel consequatur officia quaerat assumenda voluptatem blanditiis, numquam vero aliquid porro?
+                    Minima quibusdam iste maxime tenetur! Mollitia enim ratione id atque, quaerat voluptates, sapiente officiis harum nobis explicabo architecto est alias consectetur itaque tempora doloremque, ipsam quam cum perferendis. Quisquam, corporis!
+                    Vel repellat veritatis explicabo impedit non sit corrupti modi labore aliquam ipsum numquam nemo ipsam saepe, ipsa vero, dolor eaque fuga! Dicta esse ea temporibus eum sit dolorem expedita pariatur?
+                    Nemo non nulla, beatae culpa, excepturi, incidunt dolor consequatur error nostrum repellat quos reiciendis fugit? Aut libero, molestiae odio magnam nobis quia sunt ipsum fugiat recusandae, asperiores labore ea necessitatibus.</p>
                 </div>
             </div>
             <div className="row mt-4 border-top border-danger">
