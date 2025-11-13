0458752b577b9286ab62975f00e8f7d6347c4c97
Danloveper <danloveper@gmail.com>
2025-11-13 19:00:25 +0000

    add/ write features  of card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
-import storeReducer from '../store';
-import useGlobalReducer from '../hooks/useGlobalReducer';
+import { baseUrl } from '../pages/Home';
 
 const Card = (props) => {
     const result = props.result;
@@ -10,7 +9,29 @@ const Card = (props) => {
         <div className="card m-2" style={{ width: "18rem" }}>
             <img src="https://lumiere-a.akamaihd.net/v1/images/luke-skywalker-main_7ffe21c7.jpeg?region=130%2C147%2C1417%2C796" className="h-70" alt="..." />
             <div className="card-body">
-                <h5 className="card-title">{result.name}</h5>
+                <h5 className="card-title">{result.properties.name}</h5>
+                <p>
+                    {props.feature == "people" ?
+                    <>
+                        Gender: {result.properties.gender}<br />
+                        Hair Color: {result.properties.hair_color}<br />
+                        Eye-Color: {result.properties.eye_color}<br />
+                    </>
+                    : props.feature == "vehicles" ?
+                    <>
+                        Capacity: {result.properties.cargo_capacity}<br />
+                        Consumables: {result.properties.consumables}<br />
+                        Model: {result.properties.model}<br />
+                    </>
+                    :
+                    <>
+                        Population: {result.properties.population}<br />
+                        Climate: {result.properties.climate}<br />
+                        Orbital Period: {result.properties.orbital_period}<br />
+                    </>
+                }
+                </p>
+
                 <div className="d-flex justify-content-between">
                     <Link to={`/details/${props.feature}/${result.uid}`}>
                         <button type="button" className="btn btn-primary">Learn more!</button>
